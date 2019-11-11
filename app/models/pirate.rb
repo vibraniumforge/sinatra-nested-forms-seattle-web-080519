0ef1a1a4cd3c8311ab4b35dcdecef1246e3b0b1b
vibraniumforge <gregm9999@gmail.com>
2019-11-11 13:37:20 -0800

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+    
+    # def initialize(name, weight, height)
+    #     @name = name
+    #     @weight = weight
+    #     @height = height
+    #     @@all << self
+    # end
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,29 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+    
+    # def initialize(name, type, booty)
+    #     @name = name
+    #     @type = type
+    #     @booty = booty
+    #     @@all << self
+    # end
+    
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
+
 end
